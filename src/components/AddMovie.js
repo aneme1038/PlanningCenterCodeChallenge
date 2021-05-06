6c5aca2474c8006b1e7f9e05fd5b8a3b0dce5e48
aneme1038 <thriftman25@gmail.com>
2021-05-06 12:14:51 -0400

    Initializing First Component

---
+++ src/components/AddMovie.js
@@ -0,0 +1,16 @@
+//Packages
+import React from 'react';
+
+//Component
+class AddMovie extends React.Component {
+    render() {
+        return(
+            <div>
+                Test
+            </div>
+        )
+    }
+}
+
+//Export
+export default AddMovie;
